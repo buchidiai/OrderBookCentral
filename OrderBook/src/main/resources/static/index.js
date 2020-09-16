1e790ca5b59141e16af6432208a1dbbe78bbbeaa
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-09-16 12:51:31 -0700

    update trades page table and funtionality

---
+++ OrderBook/src/main/resources/static/index.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function () {
+    $(document).ready(function () {
+        $('#dtBasicExample').dataTable({
+
+            columnDefs: [{
+                    orderable: false,
+                    className: 'select-checkbox',
+                    targets: 0
+                }],
+            select: {
+                style: 'os',
+                selector: 'td:first-child'
+            }
+        });
+    });
+
+});
+
