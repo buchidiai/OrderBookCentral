e6736612d3417b62e14ead06f8888159b6e52b2b
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-09-17 10:05:09 -0700

    update chnages

--- OrderBook/src/main/resources/static/index.js
+++ OrderBook/src/main/resources/static/index.js
@@ -14,38 +14,6 @@ $(document).ready(function () {
             selector: 'td:first-child'
         }
     });
-
-
-    $('.table .openModal').on('click', function (e) {
-        e.preventDefault();
-        console.log('clicked modal');
-        var href = $(this).attr('href');
-        console.log(href, 'href');
-
-        $.get(href, function (trade, status, c) {
-            console.log(status);
-
-            console.log(c);
-
-        })
-
-        $('#modalQuickView').modal();
-    })
-
-
-    function openModal(id) {
-        
-           console.log(id , 'id');
-            var href = $(this).attr('href');
-             console.log(href, 'href');
-        $.ajax({
-            url: "/car/" + id,
-            success: function (response) {
-                $("#carViewModal").empty().append(response);
-                $("#carViewModal").modal("show");
-            }
-        });
-    }
 });
 
 
